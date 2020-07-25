32498de755ab044a220c71392f7404091ffb0199
Adesina-123 <adesinaoladimeji92@gmail.com>
2020-07-25 16:28:51 +0200

    more files added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Homepage from './Components/HomePage'
+import Homepage from './Components/Pages/Homepage/HomePage'
 
 import './App.scss';
 
---
+++ src/Components/Pages/Homepage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Directory from '../Directory/Directory'
+import './HomePage.scss'
+const HomePage = () => {
+    return (
+        <div className="hompage">
+            <Directory />
+        </div>
+    )
+}
+
+export default HomePage
