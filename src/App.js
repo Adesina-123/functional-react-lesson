5a7d46c22f5d3533bdbaaa94598a6cd25abc141d
Adesina-123 <adesinaoladimeji92@gmail.com>
2020-07-27 19:42:50 +0200

    update our menu items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import Homepage from './Components/Pages/Homepage/HomePage'
+import {Switch, Route} from 'react-router-dom';
+import Homepage from './Components/Pages/Homepage/HomePage';
 
 import './App.scss';
+
+const HatPage = () => {
+  return (
+    <div className="hoe">
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
+// const Topic = (props) => {
+//   console.log(props);
+//   return (
+//     <div className="hats">
+//       <h1>Topic</h1>
+//     </div>
+//   );
+// };
+// const TopicDetail = () => {
+//   return (
+//     <div className="hats">
+//       <h1>Topic Detail</h1>
+//     </div>
+//   );
+// };
 
 function App() {
   return (
     <div className="App">
-    <Homepage />
+      <Switch>
+        <Route exact path={'/'} component={Homepage} />
+        <Route path={'/hats'} component={HatPage} />
+   
+      </Switch>
     </div>
   );
 }
