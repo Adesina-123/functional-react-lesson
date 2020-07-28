f33b4516f7d13aaf1da37b4473404240685acd89
Adesina-123 <adesinaoladimeji92@gmail.com>
2020-07-28 20:44:35 +0200

    adding more files and styles

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Homepage from './Components/Pages/Homepage/HomePage';
+import Shop from './Components/Shop/Shop';
 
 import './App.scss';
-
-const HatPage = () => {
-  return (
-    <div className="hoe">
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
-// const Topic = (props) => {
-//   console.log(props);
-//   return (
-//     <div className="hats">
-//       <h1>Topic</h1>
-//     </div>
-//   );
-// };
-// const TopicDetail = () => {
-//   return (
-//     <div className="hats">
-//       <h1>Topic Detail</h1>
-//     </div>
-//   );
-// };
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path={'/'} component={Homepage} />
-        <Route path={'/hats'} component={HatPage} />
-   
+        <Route path={'/shop'} component={Shop} />
       </Switch>
     </div>
   );
